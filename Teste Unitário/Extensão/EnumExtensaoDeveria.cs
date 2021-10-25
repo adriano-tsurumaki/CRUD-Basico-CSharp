04717ddee32f9282a064bd32bda1ed5f44b804ef
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2021-10-25 10:19:01 -0300

    Criado o projeto de testes unitários

---
+++ Teste Unitário/Extensão/EnumExtensaoDeveria.cs
@@ -0,0 +1,23 @@
+﻿using CRUD___Adriano.Features.Estados.Enum;
+using CRUD___Adriano.Features.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teste_Unitário.Extensão
+{
+    [TestClass]
+    public class EnumExtensaoDeveria
+    {
+        [DataTestMethod]
+        [DataRow(EstadosBrasilEnum.AC, "Acre")]
+        [DataRow(EstadosBrasilEnum.AL, "Alagoas")]
+        [DataRow(EstadosBrasilEnum.SP, "São Paulo")]
+        [DataRow(EstadosBrasilEnum.TO, "Tocantins")]
+        public void Deveria_retornar_descricao_do_enum_EstadosBrasilEnum(EstadosBrasilEnum estadoEnum, string estadoEsperado)
+        {
+            Assert.AreEqual(estadoEnum.RetornarDescricao(), estadoEsperado);
+        }
+    }
+}
---
+++ Teste Unitário/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teste_Unitário
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
