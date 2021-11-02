66f84e6d0829c82bc642e418fe56b10b688a3f21
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2021-11-01 23:25:23 -0300

    Validando Cpf

--- CRUD - Adriano/Features/ValueObject/Cpf/MeuCpf.cs
+++ CRUD - Adriano/Features/ValueObject/Cpf/MeuCpf.cs
@@ -1,29 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CRUD___Adriano.Features.Utils;
+﻿using CRUD___Adriano.Features.Utils;
+using System;
+using System.Text.RegularExpressions;
 
 namespace CRUD___Adriano.Features.ValueObject.Cpf
 {
     public struct MeuCpf
     {
         private readonly string _valor;
+        private string _error;
 
         public string ValorFormatado 
-        { 
-            get => Convert.ToUInt64(_valor).ToString(@"000\.000\.000\-00");
+        {
+            get
+            {
+                if (_valor.Length != 11 || string.IsNullOrEmpty(_valor))
+                    return "";
+                return Convert.ToUInt64(_valor).ToString(@"000\.000\.000\-00");
+            }
+        }
+
+        public string Error
+        {
+            get => _error;
         }
 
         public MeuCpf(string valor)
         {
            _valor = valor.RetornarSomenteTextoEmNumeros();
+           _error = "";
         }
 
         public bool Valido()
         {
-            if (string.IsNullOrEmpty(_valor)) return false;
+            if (string.IsNullOrEmpty(_valor))
+            {
+                _error = "O Cpf não pode ser nulo ou vazio!";
+                return false;
+            }
+            
+            if (_valor.Length != 11)
+            {
+                _error = "O Cpf não possui exatos 11 números!";
+                return false;
+            }
+
+            if (_valor.Equals("00000000000") ||
+                _valor.Equals("11111111111") ||
+                _valor.Equals("22222222222") ||
+                _valor.Equals("33333333333") ||
+                _valor.Equals("44444444444") ||
+                _valor.Equals("55555555555") ||
+                _valor.Equals("66666666666") ||
+                _valor.Equals("77777777777") ||
+                _valor.Equals("88888888888") ||
+                _valor.Equals("99999999999"))
+            {
+                _error = "O Cpf não pode ter todos os números repetidos!";
+                return false;
+            }
+
+            if (!ValidarPorPartes(10, 9) || !ValidarPorPartes(11, 10))
+            {
+                _error = "O Cpf não é válido!";
+                return false;
+            }
 
             return true;
+        }
+
+        private bool ValidarPorPartes(int limite, int posicaoDigito)
+        {
+            var resultado = 0;
+
+            for (var i = limite; i > 1; i--)
+            {
+                if (!char.IsDigit(_valor[limite - i]))
+                    return false;
+                resultado += (_valor[limite - i] - '0') * i;
+            }
+
+            resultado = (resultado * 10 % 11) == 10 ? 0 : resultado * 10 % 11;
+            return resultado == _valor[posicaoDigito] - '0';
         }
 
         public override string ToString() =>
