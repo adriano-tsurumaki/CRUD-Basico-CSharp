517c6e672e8f4c063ae3cf3064f7f05c3ae50a32
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2021-11-03 20:53:05 -0300

    Modificando testes

--- Teste Unitário/ValueObject/Cpf/CpfDeveria.cs
+++ Teste Unitário/ValueObject/Cpf/CpfDeveria.cs
@@ -7,27 +7,34 @@ namespace Teste_Unitário.ValueObject.CpfDeveria
     public class CpfDeveria
     {
         [DataTestMethod]
+        [DataRow("###.###.!!!-@@", "")]
+        [DataRow("123.456.78#99)", "")]
+        [DataRow("78#99)", "")]
+        [DataRow("123", "")]
+        [DataRow("123.456.78#99)00", "")]
         [DataRow("123.456.78#99)0", "123.456.789-90")]
+        [DataRow("12345678990", "123.456.789-90")]
         public void A_propriedade_ValorFormatado_deve_formatar_corretamente(string valor, string valorEsperado)
         {
             MeuCpf cpf = valor;
+
+            var teste = cpf.ValorFormatado;
 
             Assert.AreEqual(valorEsperado, cpf.ValorFormatado);
         }
 
         [DataTestMethod]
-        [DataRow("", "O Cpf não pode ser nulo ou vazio!")]
-        [DataRow("123", "O Cpf não possui exatos 11 números!")]
-        [DataRow("1234567891234", "O Cpf não possui exatos 11 números!")]
-        [DataRow("11111111111", "O Cpf não pode ter todos os números repetidos!")]
-        [DataRow("37543212376", "O Cpf não é válido!")]
-        public void Deveria_retornar_mensagem_de_erro_corretamente(string valor, string erroEsperado)
+        [DataRow("", false)]
+        [DataRow("123", false)]
+        [DataRow("1234567891234", false)]
+        [DataRow("11111111111", false)]
+        [DataRow("37543212376", false)]
+        [DataRow("22893114806", true)]
+        public void Deveria_validar_cpf_corretamente(string valor, bool validacaoEsperada)
         {
             MeuCpf cpf = valor;
 
-            cpf.Valido();
-
-            Assert.AreEqual(cpf.Error, erroEsperado);
+            Assert.AreEqual(cpf.ValidarTudo(), validacaoEsperada);
         }
     }
 }
