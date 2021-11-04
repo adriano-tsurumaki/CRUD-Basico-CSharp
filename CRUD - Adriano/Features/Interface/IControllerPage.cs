9095833c8d1af033f61e80189a446d06be43b9bb
Adriano Tsurumaki <adriano.tsurumaki@gmail.com>
2021-11-04 16:53:59 -0300

    refazendo interfaces e deletando FormBase

---
+++ CRUD - Adriano/Features/Interface/IControllerPage.cs
@@ -0,0 +1,14 @@
+﻿using CRUD___Adriano.Features.Factory;
+using System.Windows.Forms;
+
+namespace CRUD___Adriano.Features.Interface
+{
+    public interface IControllerPage<T>
+    {
+        bool ValidarForm();
+
+        void AdicionarModel(ref T entidade);
+
+        IViewPage<T> RetornarFormulario();
+    }
+}
