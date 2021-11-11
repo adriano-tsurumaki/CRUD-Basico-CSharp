4b28ad9b280bb6c3185fa0e6347dd98e3f9ace77
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-11 15:18:12 -0300

    esqueci de colocar esse classe no commit anterior

---
+++ CRUD - Adriano/Features/Produto/Controller/ProdutoControllerPage.cs
@@ -0,0 +1,24 @@
+﻿using CRUD___Adriano.Features.Factory;
+using CRUD___Adriano.Features.Interface;
+using CRUD___Adriano.Features.Produto.Model;
+using CRUD___Adriano.Features.Produto.View;
+
+namespace CRUD___Adriano.Features.Produto.Controller
+{
+    public class ProdutoControllerPage : IControllerPage<ProdutoModel>
+    {
+        private readonly IViewPage<ProdutoModel> _frmCadastroProduto;
+
+        public ProdutoControllerPage()
+        {
+            _frmCadastroProduto = new FrmCadastroProduto(this);
+        }
+
+        public IViewPage<ProdutoModel> RetornarFormulario() => _frmCadastroProduto;
+
+        public void AdicionarModel(ref ProdutoModel produtoModel) =>
+            _frmCadastroProduto.BindModel(ref produtoModel);
+
+        public bool ValidarForm() => _frmCadastroProduto.ValidarComponentes();
+    }
+}
