9bdf88577c7f1a2eeb0b8d574b877a18cea52ce6
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-11 14:16:39 -0300

    produtoModel

--- CRUD - Adriano/Features/Produto/Model/ProdutoModel.cs
+++ CRUD - Adriano/Features/Produto/Model/ProdutoModel.cs
@@ -2,5 +2,12 @@
 {
     public class ProdutoModel
     {
+        public int Id { get; set; }
+        public string CodigoBarras { get; set; }
+        public bool Ativo { get; set; }
+        public string Nome { get; set; }
+        public float PrecoBruto { get; set; }
+        public float Lucro { get; set; }
+        public int Quantidade { get; set; }
     }
 }
