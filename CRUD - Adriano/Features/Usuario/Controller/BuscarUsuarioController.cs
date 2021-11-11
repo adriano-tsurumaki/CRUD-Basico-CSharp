6a72f9f2aa8add7474bc4e9c2f1953037d697d6e
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-11 14:17:07 -0300

    criado o form para buscar usuário genérico

---
+++ CRUD - Adriano/Features/Usuario/Controller/BuscarUsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using CRUD___Adriano.Features.Factory;
+
+namespace CRUD___Adriano.Features.Usuario.Controller
+{
+    public class BuscarUsuarioController<T> where T : class
+    {
+        private IControllerListarIdNome<T> _controller;
+
+        public BuscarUsuarioController(IControllerListarIdNome<T> controller)
+        {
+            _controller = controller;
+        }
+
+
+    }
+}
---
+++ CRUD - Adriano/Features/Usuario/View/FrmBuscarUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CRUD___Adriano.Features.Usuario.View
+{
+    public partial class FrmBuscarUsuario : Form
+    {
+        public FrmBuscarUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
