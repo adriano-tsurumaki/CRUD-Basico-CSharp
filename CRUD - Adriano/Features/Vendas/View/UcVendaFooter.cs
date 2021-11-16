9541f0347c2879bd5e7bfdfa70f1e4d8a2515c5f
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-16 17:56:25 -0300

    UcVendaFooter

---
+++ CRUD - Adriano/Features/Vendas/View/UcVendaFooter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CRUD___Adriano.Features.Vendas.View
+{
+    public partial class UcVendaFooter : UserControl
+    {
+        public UcVendaFooter()
+        {
+            InitializeComponent();
+        }
+    }
+}
