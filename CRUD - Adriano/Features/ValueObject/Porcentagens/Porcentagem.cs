4c23688b78a2756850336f214b929062c6b836ca
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-16 16:59:39 -0300

    Corrigindo value object Porcentagem

--- CRUD - Adriano/Features/ValueObject/Porcentagens/Porcentagem.cs
+++ CRUD - Adriano/Features/ValueObject/Porcentagens/Porcentagem.cs
@@ -5,24 +5,24 @@ namespace CRUD___Adriano.Features.ValueObject.Porcentagens
 {
     public struct Porcentagem
     {
-        private readonly float _valor;
+        private readonly double _valor;
 
-        public float Valor { get => _valor; }
+        public double Valor { get => _valor; }
 
         public string Formatado { get => $"{_valor} %"; }
 
-        public Porcentagem(int valor)
-        {
-            _valor = (float)Math.Round(valor / 100f, 2);
-        }
+        public Porcentagem(int valor) => _valor = valor;
+
+        public Porcentagem(double valor) => _valor = Math.Round(valor * 100, 2);
 
         // TODO: Aplicar formatação da string correta na porcentagem e os testes unitários
         public Porcentagem(string valor)
         {
-            _valor = valor.Replace("%", "").Trim().FloatOuZero();
+            _valor = valor.Replace("%", "").Trim().DoubleOuZero();
         }
 
         public static implicit operator Porcentagem(int valor) => new Porcentagem(valor);
+        public static implicit operator Porcentagem(double valor) => new Porcentagem(valor);
         public static implicit operator Porcentagem(string valor) => new Porcentagem(valor);
     }
 }
