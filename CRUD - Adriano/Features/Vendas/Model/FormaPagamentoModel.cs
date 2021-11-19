d46848bd2a191b6bf368b96e0418a1d0de1029fb
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-11-19 16:10:34 -0300

    Models criados

---
+++ CRUD - Adriano/Features/Vendas/Model/FormaPagamentoModel.cs
@@ -0,0 +1,14 @@
+﻿using CRUD___Adriano.Features.ValueObject.Precos;
+using CRUD___Adriano.Features.Vendas.Enum;
+
+namespace CRUD___Adriano.Features.Vendas.Model
+{
+    public class FormaPagamentoModel
+    {
+        public int PosicaoPagamento { get; set; }
+        public Preco ValorAPagar { get; set; }
+        public TipoPagamentoEnum TipoPagamento { get; set; }
+        public int QuantidadeParcelas { get; set; }
+        public string PosicaoParcela { get; set; }
+    }
+}
---
+++ CRUD - Adriano/Features/Vendas/Model/VendaModel.cs
@@ -0,0 +1,22 @@
+﻿using CRUD___Adriano.Features.Cadastro.Produto.Model;
+using CRUD___Adriano.Features.Colaborador.Model;
+using CRUD___Adriano.Features.ValueObject.Precos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUD___Adriano.Features.Vendas.Model
+{
+    public class VendaModel
+    {
+        public ColaboradorModel Colaborador { get; set; }
+        public ClienteModel Cliente { get; set; }
+        public IList<VendaProdutoModel> ListaDeProdutos { get; set; }
+        public IList<FormaPagamentoModel> ListaPagamentos { get; set; }
+        public Preco ValorTotal { get => ListaDeProdutos.Sum(x => x.PrecoLiquido.Valor); }
+
+        public VendaModel()
+        {
+            ListaDeProdutos = new List<VendaProdutoModel>();
+        }
+    }
+}
