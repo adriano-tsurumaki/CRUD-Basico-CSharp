a1ab94fd962be077f85c539e4ec60da9a5678591
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-12-02 17:53:49 -0300

    gridViewExtension

---
+++ CRUD - Adriano/Features/Utils/GridViewExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CRUD___Adriano.Features.Utils
+{
+    public static class GridViewExtension
+    {
+        public static DataGridViewTextBoxColumn TextBoxColumnPadrao(DataGridViewCell cellTemplate, string nome, string nomeDaPropriedade, bool apenasLeitura) =>
+            new DataGridViewTextBoxColumn()
+            {
+                CellTemplate = cellTemplate,
+                Name = nome,
+                DataPropertyName = nomeDaPropriedade,
+                DefaultCellStyle = new DataGridViewCellStyle
+                {
+                    Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point),
+                    ForeColor = Color.DodgerBlue,
+                    BackColor = Color.FromArgb(23, 31, 32),
+                    SelectionBackColor = Color.LightSeaGreen,
+                    Padding = new Padding(2)
+                },
+                ReadOnly = apenasLeitura,
+            };
+    }
+}
