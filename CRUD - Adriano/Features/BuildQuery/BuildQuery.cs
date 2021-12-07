17e5f5feb1e44e6ab7cb8dd7695dd1c74b15b840
Adriano Tsurumaki <adrianotsurumaki@gmail.com>
2021-12-07 18:00:09 -0300

    Primeiro commit do BuildQuery

---
+++ CRUD - Adriano/Features/BuildQuery/BuildQuery.cs
@@ -0,0 +1,76 @@
+﻿using CRUD___Adriano.Features.Cadastro.Produto.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CRUD___Adriano.Features.BuildQuery
+{
+    public class BuildQuery<T> where T : class, new()
+    {
+        private IList<PropertyInfo> propriedadesDaTabela;
+
+        public BuildQuery()
+        {
+            propriedadesDaTabela = new List<PropertyInfo>();
+        }
+
+        public BuildQuery<T> Select<TProperty>(Expression<Func<T, TProperty>> propriedade)
+        {
+            Type tipo = typeof(T);
+
+            var membro = propriedade.Body as MemberExpression;
+
+            if (membro == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propriedade.ToString()));
+
+            var informacaoDaPropriedade = membro.Member as PropertyInfo; 
+            if (informacaoDaPropriedade == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propriedade.ToString()));
+
+            if (tipo != informacaoDaPropriedade.ReflectedType &&
+                !tipo.IsSubclassOf(informacaoDaPropriedade.ReflectedType))
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property that is not from type {1}.",
+                    propriedade.ToString(),
+                    tipo));
+
+            propriedadesDaTabela.Add(informacaoDaPropriedade);
+
+            return this;
+        }
+
+        public BuildQuery<T> SelectOut<TOtherTable>(Expression<Func<TOtherTable, IComparable>> propriedade)
+        {
+            Type tipo = typeof(TOtherTable);
+
+            var membro = propriedade.Body as MemberExpression;
+
+            if (membro == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propriedade.ToString()));
+
+            var informacaoDaPropriedade = membro.Member as PropertyInfo;
+            if (informacaoDaPropriedade == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propriedade.ToString()));
+
+            if (tipo != informacaoDaPropriedade.ReflectedType &&
+                !tipo.IsSubclassOf(informacaoDaPropriedade.ReflectedType))
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property that is not from type {1}.",
+                    propriedade.ToString(),
+                    tipo));
+
+            propriedadesDaTabela.Add(informacaoDaPropriedade);
+
+            return this;
+        }
+    }
+}
---
+++ Teste Unitário/BuildQuery/BuildQueryDeveria.cs
@@ -0,0 +1,19 @@
+﻿using CRUD___Adriano.Features.BuildQuery;
+using CRUD___Adriano.Features.Cadastro.Produto.Model;
+using CRUD___Adriano.Features.Entidades.Endereco.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teste_Unitário.BuildQuery
+{
+    [TestClass]
+    public class BuildQueryDeveria
+    {
+        [TestMethod]
+        public void Teste()
+        {
+            var query = new BuildQuery<ClienteModel>()
+                .Select(c => c.Cpf)
+                .SelectOut<EnderecoModel>(e => e.Bairro);
+        }
+    }
+}
